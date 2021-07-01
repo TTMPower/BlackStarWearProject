a215843b69e160f5403152b56b07e329e0cc7d01
Vladislav Vishyankov <84133235+TTMPower@users.noreply.github.com>
2021-07-01 09:59:00 +0600

    Initial Commit

---
+++ BlackStarWearProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BlackStarWearProject
+//
+//  Created by Владислав Вишняков on 01.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
